3a42272a029c369e38219028a58fc3ff7ee43662
Joel Jang <joelj@mosaic-cirrascale-06.reviz.ai2.in>
2023-10-18 13:39:40 -0700

    feature viewer

--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -4,6 +4,9 @@ import pickle
 import cv2
 import os.path as osp
 import time
+import isaac_utils
+from isaacgym import gymapi
+import math
 
 from dataclasses import dataclass
 import hydra
@@ -19,6 +22,8 @@ from rsl_rl.runners import OnPolicyRunner
 
 import numpy as np
 import torch
+
+web_viewer = isaac_utils.WebViewer()
 
 @dataclass
 class PlayScriptConfig:
@@ -71,7 +76,61 @@ def main(cfg: PlayScriptConfig):
 
     log.info(f"4. Preparing environment and runner.")
     task_cfg = cfg.task
-    env: A1 = hydra.utils.instantiate(task_cfg)
+    #env: A1 = hydra.utils.instantiate(task_cfg)
+
+    gym = gymapi.acquire_gym()
+    sim_params = gymapi.SimParams()
+    sim_params.up_axis = gymapi.UP_AXIS_Z
+    sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
+    sim_params.physx.solver_type = 1
+    sim_params.physx.num_position_iterations = 4
+    sim_params.physx.num_velocity_iterations = 1
+    sim_params.physx.use_gpu = True
+    sim_params.use_gpu_pipeline = True
+    sim = gym.create_sim(compute_device=0, graphics_device=0, type=gymapi.SIM_PHYSX, params=sim_params)
+    num_envs = 1
+    spacing = 2.0
+    env_lower = gymapi.Vec3(-spacing, -spacing, 0.0)
+    env_upper = gymapi.Vec3(spacing, 0.0, spacing)
+
+    # add ground plane
+    plane_params = gymapi.PlaneParams()
+    plane_params.normal = gymapi.Vec3(0.0, 0.0, 1.0)
+    gym.add_ground(sim, plane_params)
+
+    envs = []
+    cameras = []
+
+    for i in range(num_envs):
+        # create env
+        env = gym.create_env(sim, env_lower, env_upper, int(math.sqrt(num_envs)))
+
+        # add sphere
+        pose = gymapi.Transform()
+        pose.p, pose.r = gymapi.Vec3(0.0, 0.0, 1.0), gymapi.Quat(0.0, 0.0, 0.0, 1.0)
+        gym.create_actor(env, gym.create_sphere(sim, 0.2, None), pose, "sphere", i, 0)
+
+        # add camera
+        cam_props = gymapi.CameraProperties()
+        cam_props.width, cam_props.height = 300, 300
+        cam_handle = gym.create_camera_sensor(env, cam_props)
+        gym.set_camera_location(cam_handle, env, gymapi.Vec3(1, 1, 1), gymapi.Vec3(0, 0, 0))
+
+        envs.append(env)
+        cameras.append(cam_handle)
+
+    #gym = gymapi.acquire_gym()
+    #cam_props = gymapi.CameraProperties()
+    #gym.create_camera_sensor(env, cam_props)
+    web_viewer.setup(gym, sim, envs, cameras)
+    for i in range(100000):
+        gym.simulate(sim)
+        # render the scene
+        web_viewer.render(fetch_results=True,
+                        step_graphics=True,
+                        render_all_camera_sensors=True,
+                        wait_for_page_load=True)
+    exit()
     env.reset()
     obs = env.get_observations()
     runner: OnPolicyRunner = hydra.utils.instantiate(cfg.train, env=env, _recursive_=False)
@@ -87,10 +146,13 @@ def main(cfg: PlayScriptConfig):
         log.info(f"Exported policy as jit script to: {cfg.checkpoint_root}")
 
     log.info(f"6. Running interactive play script.")
+    
     current_time = time.time()
     num_steps = int(cfg.episode_length_s / env.dt)
     for i in range(num_steps):
         actions = policy(obs.detach())
+        log.info(f"{actions}")
+        
         obs, _, _, _, infos, *_ = env.step(actions.detach())
         
         duration = time.time() - current_time
